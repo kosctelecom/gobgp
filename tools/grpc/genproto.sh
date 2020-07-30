0cbd76599debec33e8254c385871049ff3b2d2cd
Valli A. Vallimamod <vma@sip.solutions>
2020-07-30 17:39:23 +0200

    updates protoc version used in check

--- tools/grpc/genproto.sh
+++ tools/grpc/genproto.sh
@@ -13,8 +13,8 @@ if ! [[ "$0" =~ "tools/grpc/genproto.sh" ]]; then
 	exit 255
 fi
 
-if ! [[ $(protoc --version) =~ "3.7.1" ]]; then
-	echo "could not find protoc 3.7.1, is it installed + in PATH?"
+if ! [[ $(protoc --version) =~ 3.11.[0-9]+ ]]; then
+	echo "could not find protoc 3.11.x, is it installed + in PATH?"
 	exit 255
 fi
 
